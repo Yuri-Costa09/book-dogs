18ae7806cddde649ff80ded379dcee798873431b
yuri-costa-flooxai <yurircostawork@gmail.com>
2025-07-03 00:04:16 -0300

    feat: update BookSession model with new properties and constructor, Removed Book Model from Project

--- src/BookDog.Domain/Entities/BookSession.cs
+++ src/BookDog.Domain/Entities/BookSession.cs
@@ -5,24 +5,27 @@ namespace BookDog.Domain.Entities
     public class BookSession : BaseEntity
     {
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Subtitle { get; set; }
+        public int PageCount { get; set; }
+        public string ImageUrl { get; set; }
         public int TotalTimeRead { get; set; }
         public int TotalPagesRead { get; set; }
         public bool IsVisible { get; set; }
         
-        public Book Book { get; set; }
         public Tag? Tag { get; set; }
         public User User { get; set; }
         public List<Like> Likes { get; set; } = new List<Like>();
 
-        public BookSession(Book book, User user, string name, string description) 
+        public BookSession(User user, string name, string subtitle,
+            string imageUrl) 
         {
             Name = name;
-            Description = description;
+            Subtitle = subtitle;
+            PageCount = 0;
+            ImageUrl = imageUrl;
             TotalTimeRead = 0;
             TotalPagesRead = 0;
             IsVisible = true;
-            Book = book;
             User = user;
         }
     }
